45371bcc3279fc6d190fef40d75986a7fdef3ecd
Nicomagradfahren <152404188+Nicomagradfahren@users.noreply.github.com>
2024-02-07 20:04:59 +0000

    pauliihrelösung

---
+++ main.py
@@ -0,0 +1,27 @@
+import requests
+import bs4
+userAgent = {
+    'User-Agent':
+    'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'
+}
+
+url ="https://www.transfermarkt.com/bundesliga/startseite/wettbewerb/L1"
+
+r = requests.get(url, headers=userAgent)
+htmlText = r.text
+htmlDocument = bs4.BeautifulSoup(htmlText, "html.parser")
+
+table = htmlDocument.find('table', {'class': 'items'}).find('tbody')
+teams = table.find_all('tr')
+
+for i in range(len(teams)):
+    team = teams[i]
+    name = team.find('td', {'class': 'hauptlink no-border-links'}).text.strip()
+    info = team.find_all("td", {"class": "zentriert"})
+    squad = info[1].text
+    age = info[2].text
+    foreigners = info[3].text
+    marketValue = team.find_all("td", {"class": "rechts"})
+    averageMV = marketValue[0].text
+    totalMV = marketValue[1].text.strip()
+    print(f"{i}:{name} {squad} {age} {foreigners} {averageMV} {totalMV}")
